0c4f72cad9006c78608802bf6a94d3ac1f90c9b4
Nathan Knopp <kanathan@gmail.com>
2022-07-15 22:07:09 -0700

    Day 1

---
+++ src/day_1/main.rs
@@ -0,0 +1,37 @@
+
+
+fn main() {
+    let input = include_str!("input");
+    let vals = parse(input);
+    println!("{}",get_sum(&vals, 1));
+    println!("{}",get_sum(&vals, vals.len()/2));
+}
+
+
+fn parse(input: &str) -> Vec<i32> {
+    input.chars()
+        .map(|c| c.to_digit(10).unwrap() as i32)
+        .collect()
+}
+
+
+fn get_sum(input: &Vec<i32>, step: usize) -> i32 {
+    let mut sum = 0;
+    for idx in 0..input.len() {
+        if input[idx] == input[(idx + step) %input.len()] {
+            sum += input[idx];
+        }
+    }
+    sum
+}
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_1() {
+        assert_eq!(1, 1);
+    }
+}
